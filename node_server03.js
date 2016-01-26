fc9a43935fd5c62f3be9d651c7030a386d4fa9bb
wednesday_developer <wednesday.developer@gmail.com>
2016-01-26 12:46:19 +0000

    first commit

---
+++ node_server03.js
@@ -0,0 +1,13 @@
+var port = process.env.PORT;
+var io = require('socket.io').listen(port);
+console.log((new Date()) + " Server is listening on port " + port);
+
+io.sockets.on('connection', function(socket) {
+  socket.on('message', function(message) {
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('disconnect', function() {
+    socket.broadcast.emit('user disconnected');
+  });
+});
